49048ca14e2ca1b6c645ee302f2a77405cc3f72a
iluckyday <luckyday1990@mail.com>
2020-01-06 15:38:25 +0000

    test

--- travisci/ffsend.sh
+++ travisci/ffsend.sh
@@ -4,5 +4,5 @@ ffsend_ver="$(curl -skL https://api.github.com/repos/timvisee/ffsend/releases/la
 curl -skL -o /bin/ffsend https://github.com/timvisee/ffsend/releases/download/"$ffsend_ver"/ffsend-"$ffsend_ver"-linux-x64-static
 chmod +x /bin/ffsend
 
-FFSEND_URL=$(ffsend -Ifyq upload /tmp/devstack.cmp.img)
+FFSEND_URL=$(ffsend -Ifyq --verbose upload $1)
 curl -skL "http://wxpusher.zjiecode.com/api/send/message/?appToken=$WXPUSHER_APPTOKEN&uid=$WXPUSHER_UID&content=$FFSEND_URL"
