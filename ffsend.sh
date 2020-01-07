e15b41d2a3e46d6dde3af479410e59598cc9a4ba
iluckyday <luckyday1990@mail.com>
2020-01-07 06:40:39 +0000

    test

--- ffsend.sh
+++ ffsend.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 set -e
 
-cd /tmp
-
 ffsend_ver="$(curl -skL https://api.github.com/repos/timvisee/ffsend/releases/latest | grep -oP '"tag_name": "\K(.*)(?=")')"
 curl -skL -o /tmp/ffsend https://github.com/timvisee/ffsend/releases/download/"$ffsend_ver"/ffsend-"$ffsend_ver"-linux-x64-static
 chmod +x /tmp/ffsend
 
+cd /tmp
 split -d -b 800M /tmp/devstack.cmp.img devstack.cmp.img.
 
 for file in /tmp/devstack.cmp.img.*;
