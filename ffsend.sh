2d925651898f666160c4fb4095ea579712bd86e1
iluckyday <luckyday1990@mail.com>
2020-01-07 02:50:33 +0000

    test

--- ffsend.sh
+++ ffsend.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -e
+set -ex
 
 ffsend_ver="$(curl -skL https://api.github.com/repos/timvisee/ffsend/releases/latest | grep -oP '"tag_name": "\K(.*)(?=")')"
 curl -skL -o /bin/ffsend https://github.com/timvisee/ffsend/releases/download/"$ffsend_ver"/ffsend-"$ffsend_ver"-linux-x64-static
