63ddfd497de66a6d59cc9981de8bd417488f70c0
iluckyday <luckyday1990@mail.com>
2020-01-07 03:04:54 +0000

    test

--- ffsend.sh
+++ ffsend.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-set -ex
+set -e
 
 ffsend_ver="$(curl -skL https://api.github.com/repos/timvisee/ffsend/releases/latest | grep -oP '"tag_name": "\K(.*)(?=")')"
-curl -skL -o /bin/ffsend https://github.com/timvisee/ffsend/releases/download/"$ffsend_ver"/ffsend-"$ffsend_ver"-linux-x64-static
-chmod +x /bin/ffsend
+curl -skL -o /tmp/ffsend https://github.com/timvisee/ffsend/releases/download/"$ffsend_ver"/ffsend-"$ffsend_ver"-linux-x64-static
+chmod +x /tmp/ffsend
 
-FFSEND_URL=$(ffsend -Ifyq upload $1)
+FFSEND_URL=$(/tmp/ffsend -Ifyq upload $1)
 data=${FFSEND_URL/\#/%23}
 curl -skL "http://wxpusher.zjiecode.com/api/send/message/?appToken=$WXPUSHER_APPTOKEN&uid=$WXPUSHER_UID&content=$data"
