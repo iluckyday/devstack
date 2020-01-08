bd89af7b95ac9e1c50017613a7859c14a2d3f00a
iluckyday <luckyday1990@mail.com>
2020-01-08 06:26:08 +0000

    test

--- travisci/build.sh
+++ travisci/build.sh
@@ -205,7 +205,7 @@ find /usr/share/locale -mindepth 1 -maxdepth 1 ! -name 'en' -delete 2>/dev/null
 rm -rf /etc/libvirt/qemu/networks/autostart/default.xml /usr/share/doc/* /usr/share/man/*
 EOF
 
-sed -i -e 's/4096/16384/' `python3 -c "import os,diskimage_builder; print(os.path.dirname(diskimage_builder.__file__))"`/lib/disk-image-create
+sed -i 's/4096/16384/' `python3 -c "import os,diskimage_builder; print(os.path.dirname(diskimage_builder.__file__))"`/lib/disk-image-create
 
 #DIB_QUIET=1 \
 DIB_DEBUG_TRACE=1 \
@@ -227,7 +227,7 @@ DIB_DEV_USER_PASSWORD=stack \
 DIB_DEV_USER_SHELL=/bin/bash \
 DIB_DEV_USER_AUTHORIZED_KEYS=$WORKDIR/files/authorized_keys \
 DIB_DEV_USER_PWDLESS_SUDO=yes \
-disk-image-create -o $WORKDIR vm block-device-mbr cleanup-kernel-initrd devuser ubuntu-minimal
+disk-image-create -o $WORKDIR vm block-device-mbr cleanup-kernel-initrd devuser ubuntu-minimal devstack
 exit 1
 
 #qemu-system-x86_64 -name devstack-building -machine q35,accel=kvm -cpu host -smp "$(nproc)" -m 6G -nographic -object rng-random,filename=/dev/urandom,id=rng0 -device virtio-rng-pci,rng=rng0 -boot c -drive file=$WORKDIR.qcow2,if=virtio,format=qcow2,media=disk -netdev user,id=n0,ipv6=off -device virtio-net,netdev=n0
