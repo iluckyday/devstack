e3750fb779d58dd66d9424fb7473b686c313778c
iluckyday <luckyday1990@mail.com>
2020-03-25 13:53:45 +0800

    test

--- travisci/build.sh
+++ travisci/build.sh
@@ -164,6 +164,7 @@ set -ex
 
 sudo apt update
 sudo DEBIAN_FRONTEND=noninteractive apt install -y git python3-distutils python3-pip
+sudo ln -s pip3 /usr/bin/pip
 
 git clone -b $DEVSTACK_BRANCH --depth=1 https://opendev.org/openstack/devstack /tmp/devstack
 cp /home/stack/.devstack-local.conf /tmp/devstack/local.conf
