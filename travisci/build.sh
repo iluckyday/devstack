598be877b8c6be066436c5ebaee214b401836556
iluckyday <luckyday1990@mail.com>
2020-08-29 16:44:59 +0800

    test

--- travisci/build.sh
+++ travisci/build.sh
@@ -114,7 +114,6 @@ path-exclude /lib/modules/*/net/atm*
 path-exclude /lib/modules/*/net/psample*
 path-exclude /lib/modules/*/net/rose*
 path-exclude /lib/modules/*/net/ax25*
-path-exclude /lib/modules/*/net/8021q*
 path-exclude /lib/modules/*/net/9p*
 path-exclude /lib/modules/*/net/bluetooth*
 path-exclude /lib/modules/*/net/ife*
@@ -369,8 +368,7 @@ systemctl set-default last.target
 systemctl disable $disable_services
 
 apt update
-#apt install -y -o APT::Install-Recommends=0 -o APT::Install-Suggests=0 linux-image-kvm extlinux initramfs-tools
-apt install -y -o APT::Install-Recommends=0 -o APT::Install-Suggests=0 linux-image-generic extlinux initramfs-tools
+apt install -y -o APT::Install-Recommends=0 -o APT::Install-Suggests=0 linux-image-kvm extlinux initramfs-tools
 dd if=/usr/lib/EXTLINUX/mbr.bin of=$loopx
 extlinux -i /boot/syslinux
 rm -rf /var/log/* /tmp/* /var/tmp/*
