a3904d03682aa41d0cf0f54f60f9e94edd9bfe06
iluckyday <luckyday1990@mail.com>
2020-09-20 18:16:43 +0800

    test

--- transfer.sh
+++ transfer.sh
@@ -9,6 +9,6 @@ FILENAME=$(basename $f)
 SIZE=$(du -h $f | awk '{print $1}')
 cow_data=$(/tmp/transfer cow $f)
 cow_url=$(echo ${cow_data} | cut -d ' ' -f5)
-[[ ! -z "$cow_url" ]] && exit 1
+[[ -z "$cow_url" ]] && exit 1
 data="$FILENAME-$SIZE-${cow_url}"
 curl -skLo /dev/null "https://wxpusher.zjiecode.com/api/send/message/?appToken=${WXPUSHER_APPTOKEN}&uid=${WXPUSHER_UID}&content=${data}"
