171caf007b26055e019f1eba5dbd0fcb32f6887e
iluckyday <luckyday1990@mail.com>
2020-09-20 10:58:09 +0800

    test

--- travisci/build.sh
+++ travisci/build.sh
@@ -109,7 +109,6 @@ path-exclude /lib/modules/*/net/netrom*
 path-exclude /lib/modules/*/net/lapb*
 path-exclude /lib/modules/*/net/mac80211*
 path-exclude /lib/modules/*/net/6lowpan*
-path-exclude /lib/modules/*/net/sunrpc*
 path-exclude /lib/modules/*/net/rxrpc*
 path-exclude /lib/modules/*/net/atm*
 path-exclude /lib/modules/*/net/psample*
@@ -276,6 +275,11 @@ EOF
 
 cat << EOF > ${mount_dir}/home/stack/.devstack-local.conf
 [[local|localrc]]
+enable_plugin amqp1 https://opendev.org/openstack/devstack-plugin-amqp1
+AMQP1_USERNAME=qpid
+AMQP1_PASSWORD=devstack
+AMQP1_SERVICE=qpid-dual
+
 disable_service tempest dstat
 disable_service c-sch c-api c-vol
 disable_service horizon
