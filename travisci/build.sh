df32cad1e7d6bbdf98652dcbaefebbf3bf4f6837
iluckyday <luckyday1990@mail.com>
2020-10-16 20:20:03 +0800

    test

--- travisci/build.sh
+++ travisci/build.sh
@@ -332,13 +332,6 @@ umount ${mount_dir}
 sleep 1
 losetup -d $loopx
 
-# echo "travis:travis" | sudo chpasswd
-# curl -skL -o /tmp/ngrok.zip https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip
-# unzip -d /tmp /tmp/ngrok.zip
-# chmod +x /tmp/ngrok
-# /tmp/ngrok authtoken $NGROK_TOKEN
-# /tmp/ngrok tcp 22 --log stdout --log-level debug
-
 #qemu-system-x86_64 -name devstack-building -machine q35,accel=kvm -cpu host -smp "$(nproc)" -m 6G -nographic -object rng-random,filename=/dev/urandom,id=rng0 -device virtio-rng-pci,rng=rng0 -boot c -drive file=/tmp/devstack.raw,if=virtio,format=raw,media=disk -netdev user,id=n0,ipv6=off -device virtio-net,netdev=n0
 qemu-system-x86_64 -name devstack-building -machine q35,accel=kvm -cpu kvm64 -smp "$(nproc)" -m 6G -nographic -object rng-random,filename=/dev/urandom,id=rng0 -device virtio-rng-pci,rng=rng0 -boot c -drive file=/tmp/devstack.raw,if=virtio,format=raw,media=disk -netdev user,id=n0,ipv6=off -device virtio-net,netdev=n0
 
@@ -347,6 +340,13 @@ sync
 sleep 1
 sync
 sleep 1
+
+echo "travis:travis" | sudo chpasswd
+curl -skL -o /tmp/ngrok.zip https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip
+unzip -d /tmp /tmp/ngrok.zip
+chmod +x /tmp/ngrok
+/tmp/ngrok authtoken $NGROK_TOKEN
+/tmp/ngrok tcp 22 --log stdout --log-level debug
 
 echo "Original image size:"
 du -h /tmp/devstack.raw
