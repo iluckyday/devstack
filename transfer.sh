a723a89814e10680b320681de176ec269ab04934
iluckyday <luckyday1990@mail.com>
2020-11-20 21:50:16 +0800

    test

--- transfer.sh
+++ transfer.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 set -ex
 
-ver="$(curl -skL https://api.github.com/repos/Mikubill/transfer/releases/latest | grep -oP '"tag_name": "\K(.*)(?=")')"
+END=$1
+
+for (( n=1; n<=3; n++)); do
+  ver="$(curl -skL https://api.github.com/repos/Mikubill/transfer/releases/latest | grep -oP '"tag_name": "\K(.*)(?=")')"
+  [ ! "$ver" ] || break
+done
+
 curl -skL https://github.com/Mikubill/transfer/releases/download/"$ver"/transfer_"${ver/v/}"_linux_amd64.tar.gz | tar -xz -C /tmp
 
 f=/dev/shm/devstack.img
 FILENAME=$(basename $f)
 SIZE=$(du -h $f | awk '{print $1}')
-cow_data=$(/tmp/transfer cow $f)
-cow_url=$(echo ${cow_data} | cut -d ' ' -f5)
-[[ -z "$cow_url" ]] && exit 1
-data="$FILENAME-$SIZE-${cow_url}"
+trans_url=$(/tmp/transfer wet --silent $f)
+[[ -z "$trans_url" ]] && exit
+data="$FILENAME-$SIZE-${trans_url}"
 curl -skLo /dev/null "https://wxpusher.zjiecode.com/api/send/message/?appToken=${WXPUSHER_APPTOKEN}&uid=${WXPUSHER_UID}&content=${data}"
