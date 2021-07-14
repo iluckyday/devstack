0a724333ecf6edd9268395ebc3eddcdd02473b27
iluckyday <luckyday1990@mail.com>
2021-07-14 13:57:40 +0800

    test

--- transfer.sh
+++ transfer.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -ex
 
-END=$1
+FILE=$1
 
 for (( n=1; n<=3; n++)); do
   ver="$(curl -skL https://api.github.com/repos/Mikubill/transfer/releases/latest | grep -oP '"tag_name": "\K(.*)(?=")')"
@@ -10,7 +10,12 @@ done
 
 curl -skL https://github.com/Mikubill/transfer/releases/download/"$ver"/transfer_"${ver/v/}"_linux_amd64.tar.gz | tar -xz -C /tmp
 
-f=/dev/shm/devstack.img
+if [ ! -f "$FILE" ]; then
+  f=/dev/shm/devstack.img
+else
+  f=$FILE
+fi
+
 FILENAME=$(basename $f)
 SIZE=$(du -h $f | awk '{print $1}')
 trans_url=$(/tmp/transfer wet --silent $f)
