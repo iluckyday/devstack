df87d77e101f39fa93557238dfeb0ca66557282f
iluckyday <luckyday1990@mail.com>
2021-10-20 12:47:37 +0800

    test

--- cirrusci/tacker.sh
+++ cirrusci/tacker.sh
@@ -399,7 +399,7 @@ umount ${mount_dir}
 sleep 1
 losetup -d $loopx
 
-qemu-system-x86_64 -name devstack-building -machine,usb=off,accel=kvm q35 -cpu host -smp "$(nproc)" -m 24g -nographic -object rng-random,filename=/dev/urandom,id=rng0 -device virtio-rng-pci,rng=rng0 -boot c -drive file=/tmp/devstack.raw,if=virtio,format=raw,media=disk -netdev user,id=n0,ipv6=off -device virtio-net,netdev=n0
+qemu-system-x86_64 -name devstack-building -machine q35,usb=off,accel=kvm -cpu host -smp "$(nproc)" -m 24g -nographic -object rng-random,filename=/dev/urandom,id=rng0 -device virtio-rng-pci,rng=rng0 -boot c -drive file=/tmp/devstack.raw,if=virtio,format=raw,media=disk -netdev user,id=n0,ipv6=off -device virtio-net,netdev=n0
 
 sleep 1
 sync
